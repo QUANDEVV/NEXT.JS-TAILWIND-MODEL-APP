06e4470b51380c2d486317d1bc884c8effda4c23
web3Codess <vercelploy@gmail.com>
2022-06-10 13:55:56 +0300

    commit on 'origin'

--- pages/index.js
+++ pages/index.js
@@ -88,10 +88,157 @@ const Home = () => {
 
 
 
+<Link  href='klo'>
+<div className="min-w-190 minlg:min-w-240 dark:bg-nft-black-3 bg-white border dark:border-nft-black-3 border-nft-gray-1 rounded-3xl flex flex-col p-4 m-4">
+      <div className="my-2 flex justify-center">
+        <div className="relative w-40 h-40 minlg:w-28 minlg:h-28">
+          <Image
+            src={images.lenke }
+            layout="fill"
+            objectFit="cover"
+            alt="creator"
+            className="rounded-full"
+          />
+          <div className="absolute w-4 h-4 minlg:w-7 minlg:h-7 bottom-0 -right-0">
+            <Image
+              src={images.tick}
+              layout="fill"
+              objectFit="contain"
+              alt="tick"
+            />
+          </div>
+        </div>
+    </div>
+    </div>
+</Link>
+
+
+<Link  href='klo'>
+<div className="min-w-190 minlg:min-w-240 dark:bg-nft-black-3 bg-white border dark:border-nft-black-3 border-nft-gray-1 rounded-3xl flex flex-col p-4 m-4">
+      <div className="my-2 flex justify-center">
+        <div className="relative w-40 h-40 minlg:w-28 minlg:h-28">
+          <Image
+            src={images.lenke }
+            layout="fill"
+            objectFit="cover"
+            alt="creator"
+            className="rounded-full"
+          />
+          <div className="absolute w-4 h-4 minlg:w-7 minlg:h-7 bottom-0 -right-0">
+            <Image
+              src={images.tick}
+              layout="fill"
+              objectFit="contain"
+              alt="tick"
+            />
+          </div>
+        </div>
+    </div>
+    </div>
+</Link>
+
+
+
+<Link  href='klo'>
+<div className="min-w-190 minlg:min-w-240 dark:bg-nft-black-3 bg-white border dark:border-nft-black-3 border-nft-gray-1 rounded-3xl flex flex-col p-4 m-4">
+      <div className="my-2 flex justify-center">
+        <div className="relative w-40 h-40 minlg:w-28 minlg:h-28">
+          <Image
+            src={images.lenke }
+            layout="fill"
+            objectFit="cover"
+            alt="creator"
+            className="rounded-full"
+          />
+          <div className="absolute w-4 h-4 minlg:w-7 minlg:h-7 bottom-0 -right-0">
+            <Image
+              src={images.tick}
+              layout="fill"
+              objectFit="contain"
+              alt="tick"
+            />
+          </div>
+        </div>
+    </div>
+    </div>
+</Link>
+
+
+
+<Link  href='klo'>
+<div className="min-w-190 minlg:min-w-240 dark:bg-nft-black-3 bg-white border dark:border-nft-black-3 border-nft-gray-1 rounded-3xl flex flex-col p-4 m-4">
+      <div className="my-2 flex justify-center">
+        <div className="relative w-40 h-40 minlg:w-28 minlg:h-28">
+          <Image
+            src={images.lenke }
+            layout="fill"
+            objectFit="cover"
+            alt="creator"
+            className="rounded-full"
+          />
+          <div className="absolute w-4 h-4 minlg:w-7 minlg:h-7 bottom-0 -right-0">
+            <Image
+              src={images.tick}
+              layout="fill"
+              objectFit="contain"
+              alt="tick"
+            />
+          </div>
+        </div>
+    </div>
+    </div>
+</Link>
+
    
 
+<Link  href='klo'>
+<div className="min-w-190 minlg:min-w-240 dark:bg-nft-black-3 bg-white border dark:border-nft-black-3 border-nft-gray-1 rounded-3xl flex flex-col p-4 m-4">
+      <div className="my-2 flex justify-center">
+        <div className="relative w-40 h-40 minlg:w-28 minlg:h-28">
+          <Image
+            src={images.lenke }
+            layout="fill"
+            objectFit="cover"
+            alt="creator"
+            className="rounded-full"
+          />
+          <div className="absolute w-4 h-4 minlg:w-7 minlg:h-7 bottom-0 -right-0">
+            <Image
+              src={images.tick}
+              layout="fill"
+              objectFit="contain"
+              alt="tick"
+            />
+          </div>
+        </div>
+    </div>
+    </div>
+</Link>
 
 
+<Link  href='klo'>
+<div className="min-w-190 minlg:min-w-240 dark:bg-nft-black-3 bg-white border dark:border-nft-black-3 border-nft-gray-1 rounded-3xl flex flex-col p-4 m-4">
+      <div className="my-2 flex justify-center">
+        <div className="relative w-40 h-40 minlg:w-28 minlg:h-28">
+          <Image
+            src={images.lenke }
+            layout="fill"
+            objectFit="cover"
+            alt="creator"
+            className="rounded-full"
+          />
+          <div className="absolute w-4 h-4 minlg:w-7 minlg:h-7 bottom-0 -right-0">
+            <Image
+              src={images.tick}
+              layout="fill"
+              objectFit="contain"
+              alt="tick"
+            />
+          </div>
+        </div>
+    </div>
+    </div>
+</Link>
 
   
        
@@ -112,7 +259,11 @@ const Home = () => {
 
 
 </div>
-</div>
+
+
+      </div>
+    
+
 
  
 
